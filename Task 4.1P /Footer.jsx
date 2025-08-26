932d7f9bee02467323f760180f0586f6d9d79d9a
Shabad8 <shabad4911.se24@chitkara.edu.in>
2025-08-26 11:55:26 +0530

    Create Footer.jsx

---
+++ Task 4.1P /Footer.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="footer-container">
+        <div>
+          <h4>Explore</h4>
+          <p>Home</p>
+          <p>Questions</p>
+          <p>Articles</p>
+          <p>Tutorials</p>
+        </div>
+        <div>
+          <h4>Support</h4>
+          <p>FAQs</p>
+          <p>Help</p>
+          <p>Contact Us</p>
+        </div>
+        <div>
+          <h4>Stay Connected</h4>
+          <p>🌐 Facebook | 🐦 Twitter | 📸 Instagram</p>
+        </div>
+      </div>
+      <p>DEV@Deakin 2022</p>
+      <p>Privacy Policy | Terms | Code of Conduct</p>
+    </footer>
+  );
+}
+
+export default Footer;
