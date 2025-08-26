05dc586b8e75d324efe9ac98da1cef98f7175c18
Shabad8 <shabad4911.se24@chitkara.edu.in>
2025-08-26 11:56:06 +0530

    Create Hero.jsx

---
+++ Task 4.1P /Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div className="hero">
+      <img src="https://picsum.photos/1200/300" alt="Hero Banner" />
+    </div>
+  );
+}
+
+export default Hero;
